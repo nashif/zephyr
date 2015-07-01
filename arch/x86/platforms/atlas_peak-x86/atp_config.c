b1dcb602a24f8453b4aa7f2d9b27c81808b99e8b
Anas Nashif <anas.nashif@intel.com>
2015-07-01 09:23:03 -0400

    atlaspeak: add rtc configuration
    Change-Id: I1d3342a71d904cf888299cff6a88fdd2b4050c31
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- arch/x86/platforms/atlas_peak-x86/atp_config.c
+++ arch/x86/platforms/atlas_peak-x86/atp_config.c
@@ -25,3 +25,19 @@ DECLARE_DEVICE_INIT_CONFIG(pmux,			/* config name */
 pure_init(pmux, NULL);
 
 #endif /* CONFIG_PINMUX */
+
+#ifdef CONFIG_DW_RTC
+#include <rtc/dw_rtc.h>
+
+struct dw_rtc_dev_config rtc_dev = {
+	.base_address = RTC_BASE_ADDR,
+};
+
+DECLARE_DEVICE_INIT_CONFIG(rtc,
+			   RTC_DRV_NAME,
+			   &dw_rtc_init,
+			   &rtc_dev);
+
+micro_early_init(rtc, NULL);
+
+#endif
