caee396bcf7d8a5535809744eca97eddeabb5454
Anas Nashif <anas.nashif@intel.com>
2015-07-01 09:28:14 -0400

    add registers and global defines for RTC/WDT/PWM
    Change-Id: I7c87c889e81caab88d2b0fb22e15603de97c2c6f
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- arch/x86/platforms/atlas_peak-x86/board.h
+++ arch/x86/platforms/atlas_peak-x86/board.h
@@ -184,6 +184,122 @@ the Atlas Peak BSP.
 #define VIRT_POOL_MMIO_SIZE MB(1)
 
 
+
+/* Core system registers */
+
+struct scss_ccu {
+        volatile uint32_t osc0_cfg0;    /**< Hybrid Oscillator Configuration 0 */
+        volatile uint32_t osc0_stat1;   /**< Hybrid Oscillator status 1 */
+        volatile uint32_t osc0_cfg1;    /**< Hybrid Oscillator configuration 1 */
+        volatile uint32_t osc1_stat0;   /**< RTC Oscillator status 0 */
+        volatile uint32_t osc1_cfg0;    /**< RTC Oscillator Configuration 0 */
+        volatile uint32_t usb_pll_cfg0; /**< USB Phase lock look configuration */
+        volatile uint32_t
+            ccu_periph_clk_gate_ctl; /**< Peripheral Clock Gate Control */
+        volatile uint32_t
+            ccu_periph_clk_div_ctl0; /**< Peripheral Clock Divider Control 0 */
+        volatile uint32_t
+            ccu_gpio_db_clk_ctl; /**< Peripheral Clock Divider Control 1 */
+        volatile uint32_t ccu_ext_clock_ctl; /**< External Clock Control Register */
+        volatile uint32_t ccu_ss_periph_clk_gate_ctl; /**< Sensor subsustem
+                                                     peripheral clock gate
+                                                     control */
+        volatile uint32_t ccu_lp_clk_ctl; /**< System Low Power Clock Control */
+        volatile uint32_t reserved;
+        volatile uint32_t ccu_mlayer_ahb_ctl; /**< AHB Control Register */
+        volatile uint32_t ccu_sys_clk_ctl;    /**< System Clock Control Register */
+        volatile uint32_t osc_lock_0;        /**< Clocks Lock Register */
+};
+
+struct scss_peripheral {
+        volatile uint32_t usb_phy_cfg0; /**< USB Configuration */
+        volatile uint32_t periph_cfg0;  /**< Peripheral Configuration */
+        volatile uint32_t reserved[2];
+        volatile uint32_t cfg_lock; /**< Configuration Lock */
+};
+
+struct int_ss_i2c {
+        volatile uint32_t err_mask;
+        volatile uint32_t rx_avail_mask;
+        volatile uint32_t tx_req_mask;
+        volatile uint32_t stop_det_mask;
+};
+
+struct int_ss_spi {
+        volatile uint32_t err_int_mask;
+        volatile uint32_t rx_avail_mask;
+        volatile uint32_t tx_req_mask;
+};
+
+struct scss_interrupt {
+        volatile uint32_t int_ss_adc_err_mask;
+        volatile uint32_t int_ss_adc_irq_mask;
+        volatile uint32_t int_ss_gpio_intr_mask[2];
+        struct int_ss_i2c int_ss_i2c[2];
+        struct int_ss_spi int_ss_spi[2];
+        volatile uint32_t int_i2c_mst_mask[2];
+        volatile uint32_t reserved;
+        volatile uint32_t int_spi_mst_mask[2];
+        volatile uint32_t int_spi_slv_mask[1];
+        volatile uint32_t int_uart_mask[2];
+        volatile uint32_t int_i2s_mask;
+        volatile uint32_t int_gpio_mask;
+        volatile uint32_t int_pwm_timer_mask;
+        volatile uint32_t int_usb_mask;
+        volatile uint32_t int_rtc_mask;
+        volatile uint32_t int_watchdog_mask;
+        volatile uint32_t int_dma_channel_mask[8];
+        volatile uint32_t int_mailbox_mask;
+        volatile uint32_t int_comparators_ss_halt_mask;
+        volatile uint32_t int_comparators_host_halt_mask;
+        volatile uint32_t int_comparators_ss_mask;
+        volatile uint32_t int_comparators_host_mask;
+        volatile uint32_t int_host_bus_err_mask;
+        volatile uint32_t int_dma_error_mask;
+        volatile uint32_t int_sram_controller_mask;
+        volatile uint32_t int_flash_controller_mask[2];
+        volatile uint32_t int_aon_timer_mask;
+        volatile uint32_t int_adc_pwr_mask;
+        volatile uint32_t int_adc_calib_mask;
+        volatile uint32_t int_aon_gpio_mask;
+        volatile uint32_t lock_int_mask_reg;
+};
+
+#define SCSS_PERIPHERAL_BASE (0xB0800800)
+#define SCSS_PERIPHERAL ((struct scss_peripheral *)SCSS_PERIPHERAL_BASE)
+
+#define SCSS_INT_BASE (0xB0800400)
+#define SCSS_INTERRUPT ((struct scss_interrupt *)SCSS_INT_BASE)
+
+/* Base Register */
+#define SCSS_REGISTER_BASE		0xB0800000
+#define SCSS_CCU ((struct scss_ccu *)SCSS_REGISTER_BASE)
+
+/* Peripheral Clock Gate Control */
+#define SCSS_CCU_PERIPH_CLK_GATE_CTL	0x18
+#define CCU_PERIPH_CLK_EN 		(1 << 1)
+#define CCU_PERIPH_CLK_DIV_CTL0		0x1C
+#define INT_UNMASK_IA  		        (~0x00000001)
+
+/* PWM */
+#define CCU_PWM_PCLK_EN_SW     		(1 << 12)
+
+/* Watchdog */
+#define WDT_BASE_ADDR               	0xB0000000
+#define INT_WDT_IRQ               	0xc
+#define INT_WATCHDOG_MASK		0x47C
+#define SCSS_PERIPH_CFG0     		0x804
+#define SCSS_PERIPH_CFG0_WDT_ENABLE	(1 << 1)
+#define CCU_WDT_PCLK_EN_SW     		(1 << 10)
+
+/* RTC */
+#define RTC_BASE_ADDR               	0xB0000400
+#define SCSS_INT_RTC_MASK	        0x478
+#define SCSS_CCU_SYS_CLK_CTL		0x38
+#define CCU_RTC_PCLK_EN_SW 		(1 << 11)
+
+#define INT_RTC_IRQ  		        0xb
+
 #ifndef _ASMLANGUAGE
 
 /**************************************************************************
