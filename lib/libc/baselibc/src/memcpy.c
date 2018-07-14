a48c04af9d9fb5b799b724db6efbc9839ee07437
Anas Nashif <anas.nashif@intel.com>
2018-07-14 11:12:52 -0500

    baselibc: use __asm__ macro
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- lib/libc/baselibc/src/memcpy.c
+++ lib/libc/baselibc/src/memcpy.c
@@ -11,12 +11,12 @@ void *memcpy(void *dst, const void *src, size_t n)
 	char *q = dst;
 #if defined(__i386__)
 	size_t nl = n >> 2;
-	asm volatile ("cld ; rep ; movsl ; movl %3,%0 ; rep ; movsb":"+c" (nl),
+	__asm__ volatile ("cld ; rep ; movsl ; movl %3,%0 ; rep ; movsb":"+c" (nl),
 		      "+S"(p), "+D"(q)
 		      :"r"(n & 3));
 #elif defined(__x86_64__)
 	size_t nq = n >> 3;
-	asm volatile ("cld ; rep ; movsq ; movl %3,%%ecx ; rep ; movsb":"+c"
+	__asm__ volatile ("cld ; rep ; movsq ; movl %3,%%ecx ; rep ; movsb":"+c"
 		      (nq), "+S"(p), "+D"(q)
 		      :"r"((uint32_t) (n & 7)));
 #elif defined(ARCH_cortex_m0) || defined(ARCH_cortex_m3) || defined(ARCH_cortex_m4) || defined(ARCH_cortex_m7)
@@ -28,7 +28,7 @@ void *memcpy(void *dst, const void *src, size_t n)
          * For Cortex-M3/4/7 we can speed up a bit by moving 32-bit words since
          * it supports unaligned access.
          */
-        asm (".syntax unified           \n"
+        __asm__ (".syntax unified           \n"
              "       b    test1         \n"
              "loop1: ldr  r3, [r1, r2]  \n"
              "       str  r3, [r0, r2]  \n"
@@ -38,7 +38,7 @@ void *memcpy(void *dst, const void *src, size_t n)
             );
 #endif
 
-        asm (".syntax unified           \n"
+        __asm__ (".syntax unified           \n"
              "       b    test2         \n"
              "loop2: ldrb r3, [r1, r2]  \n"
              "       strb r3, [r0, r2]  \n"
--- lib/libc/baselibc/src/memset.c
+++ lib/libc/baselibc/src/memset.c
@@ -11,12 +11,12 @@ void *memset(void *dst, int c, size_t n)
 
 #if defined(__i386__)
 	size_t nl = n >> 2;
-	asm volatile ("cld ; rep ; stosl ; movl %3,%0 ; rep ; stosb"
+	__asm__ volatile ("cld ; rep ; stosl ; movl %3,%0 ; rep ; stosb"
 		      : "+c" (nl), "+D" (q)
 		      : "a" ((unsigned char)c * 0x01010101U), "r" (n & 3));
 #elif defined(__x86_64__)
 	size_t nq = n >> 3;
-	asm volatile ("cld ; rep ; stosq ; movl %3,%%ecx ; rep ; stosb"
+	__asm__ volatile ("cld ; rep ; stosq ; movl %3,%%ecx ; rep ; stosb"
 		      :"+c" (nq), "+D" (q)
 		      : "a" ((unsigned char)c * 0x0101010101010101U),
 			"r" ((uint32_t) n & 7));
