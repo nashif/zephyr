d2c160040b38b3127778e9610279c2ca1276d6b3
Daniel Leung <daniel.leung@intel.com>
2020-08-05 12:53:25 -0700

    arch: common/timing: use new timing subsystem API
    Use the new timing subsystem API inside timing_info_bench.c
    under arch/common.
    
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>

--- arch/common/timing_info_bench.c
+++ arch/common/timing_info_bench.c
@@ -1,87 +1,36 @@
 /*
- * Copyright (c) 2017 Intel Corporation.
+ * Copyright (c) 2017-2020 Intel Corporation.
  *
  * SPDX-License-Identifier: Apache-2.0
  */
+
 #include <kernel.h>
 #include <kernel_internal.h>
+#include <timing/timing.h>
 
-uint64_t  arch_timing_swap_start;
-uint64_t  arch_timing_swap_end;
-uint64_t  arch_timing_irq_start;
-uint64_t  arch_timing_irq_end;
-uint64_t  arch_timing_tick_start;
-uint64_t  arch_timing_tick_end;
-uint64_t  arch_timing_enter_user_mode_end;
+timing_t arch_timing_swap_start;
+timing_t arch_timing_swap_end;
+timing_t arch_timing_irq_start;
+timing_t arch_timing_irq_end;
+timing_t arch_timing_tick_start;
+timing_t arch_timing_tick_end;
+timing_t arch_timing_enter_user_mode_end;
 
-/* location of the time stamps*/
+/* If == 1U, record timing during swap */
 uint32_t arch_timing_value_swap_end;
-uint64_t arch_timing_value_swap_common;
-
-#if defined(CONFIG_NRF_RTC_TIMER)
-#include <nrfx.h>
-
-/* To get current count of timer, first 1 need to be written into
- * Capture Register and Current Count will be copied into corresponding
- * current count register.
- */
-#define TIMING_INFO_PRE_READ()        (NRF_TIMER2->TASKS_CAPTURE[0] = 1)
-#define TIMING_INFO_OS_GET_TIME()     (NRF_TIMER2->CC[0])
-#define TIMING_INFO_GET_TIMER_VALUE() (TIMING_INFO_OS_GET_TIME())
-
-#elif defined(CONFIG_SOC_SERIES_MEC1501X)
-#define TIMING_INFO_PRE_READ()
-#define TIMING_INFO_OS_GET_TIME()     (B32TMR1_REGS->CNT)
-#define TIMING_INFO_GET_TIMER_VALUE() (TIMING_INFO_OS_GET_TIME())
-
-#elif defined(CONFIG_X86)
-#define TIMING_INFO_PRE_READ()
-#define TIMING_INFO_OS_GET_TIME()      (z_tsc_read())
-#define TIMING_INFO_GET_TIMER_VALUE()  (TIMING_INFO_OS_GET_TIME())
-
-#elif defined(CONFIG_ARC)
-#define TIMING_INFO_PRE_READ()
-#define TIMING_INFO_OS_GET_TIME()     (k_cycle_get_32())
-#define TIMING_INFO_GET_TIMER_VALUE() (z_arc_v2_aux_reg_read(_ARC_V2_TMR0_COUNT))
-
-#elif defined(CONFIG_NIOS2)
-#include "altera_avalon_timer_regs.h"
-#define TIMING_INFO_PRE_READ()         \
-	(IOWR_ALTERA_AVALON_TIMER_SNAPL(TIMER_0_BASE, 10))
-
-#define NIOS2_SUBTRACT_CLOCK_CYCLES(val)     \
-	((IORD_ALTERA_AVALON_TIMER_PERIODH(TIMER_0_BASE)	\
-	  << 16 |						\
-	  (IORD_ALTERA_AVALON_TIMER_PERIODL(TIMER_0_BASE)))	\
-	 - ((uint32_t)val))
-
-#define TIMING_INFO_OS_GET_TIME()      (NIOS2_SUBTRACT_CLOCK_CYCLES(\
-	((uint32_t)IORD_ALTERA_AVALON_TIMER_SNAPH(TIMER_0_BASE) << 16)\
-	| ((uint32_t)IORD_ALTERA_AVALON_TIMER_SNAPL(TIMER_0_BASE))))
-
-#define TIMING_INFO_GET_TIMER_VALUE()  (TIMING_INFO_OS_GET_TIME())
-
-#else
-#define TIMING_INFO_PRE_READ()
-#define TIMING_INFO_OS_GET_TIME()      (k_cycle_get_32())
-#define TIMING_INFO_GET_TIMER_VALUE()  (k_cycle_get_32())
-#endif	/* CONFIG_NRF_RTC_TIMER */
-
 
 void read_timer_start_of_swap(void)
 {
 	if (arch_timing_value_swap_end == 1U) {
-		TIMING_INFO_PRE_READ();
-		arch_timing_swap_start = TIMING_INFO_OS_GET_TIME();
+		arch_timing_swap_start = timing_counter_get();
 	}
 }
 
 void read_timer_end_of_swap(void)
 {
 	if (arch_timing_value_swap_end == 1U) {
-		TIMING_INFO_PRE_READ();
 		arch_timing_value_swap_end = 2U;
-		arch_timing_value_swap_common = TIMING_INFO_OS_GET_TIME();
+		arch_timing_swap_end = timing_counter_get();
 	}
 }
 
@@ -90,30 +39,25 @@ void read_timer_end_of_swap(void)
  */
 void read_timer_start_of_isr(void)
 {
-	TIMING_INFO_PRE_READ();
-	arch_timing_irq_start  = TIMING_INFO_GET_TIMER_VALUE();
+	arch_timing_irq_start = timing_counter_get();
 }
 
 void read_timer_end_of_isr(void)
 {
-	TIMING_INFO_PRE_READ();
-	arch_timing_irq_end  = TIMING_INFO_GET_TIMER_VALUE();
+	arch_timing_irq_end = timing_counter_get();
 }
 
 void read_timer_start_of_tick_handler(void)
 {
-	TIMING_INFO_PRE_READ();
-	arch_timing_tick_start  = TIMING_INFO_GET_TIMER_VALUE();
+	arch_timing_tick_start = timing_counter_get();
 }
 
 void read_timer_end_of_tick_handler(void)
 {
-	TIMING_INFO_PRE_READ();
-	 arch_timing_tick_end  = TIMING_INFO_GET_TIMER_VALUE();
+	arch_timing_tick_end = timing_counter_get();
 }
 
 void read_timer_end_of_userspace_enter(void)
 {
-	TIMING_INFO_PRE_READ();
-	arch_timing_enter_user_mode_end = TIMING_INFO_GET_TIMER_VALUE();
+	arch_timing_enter_user_mode_end = timing_counter_get();
 }
