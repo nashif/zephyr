1b5fe3457e446e1e827b96dac884643920e1b84d
Anas Nashif <anas.nashif@intel.com>
2020-12-06 13:34:50 -0500

    move all to maintainers/
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- scripts/maintainers/Maintainers.py
+++ scripts/maintainers/Maintainers.py
@@ -60,94 +60,6 @@ def validate(data):
     except pykwalify.errors.SchemaError as se:
         raise MalformedMaintainerFile(se.msg) from se
 
-def _main():
-    # Entry point when run as an executable
-
-    args = _parse_args()
-    try:
-        args.cmd_fn(Maintainers(args.maintainers), args)
-    except (MaintainersError, GitError) as e:
-        _serr(e)
-
-
-def _parse_args():
-    # Parses arguments when run as an executable
-
-    parser = argparse.ArgumentParser(
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-        description=__doc__)
-
-    parser.add_argument(
-        "-m", "--maintainers",
-        metavar="MAINTAINERS_FILE",
-        help="Maintainers file to load. If not specified, MAINTAINERS.yml in "
-             "the top-level repository directory is used, and must exist. "
-             "Paths in the maintainers file will always be taken as relative "
-             "to the top-level directory.")
-
-    subparsers = parser.add_subparsers(
-        help="Available commands (each has a separate --help text)")
-
-    id_parser = subparsers.add_parser(
-        "path",
-        help="List area(s) for paths")
-    id_parser.add_argument(
-        "paths",
-        metavar="PATH",
-        nargs="*",
-        help="Path to list areas for")
-    id_parser.set_defaults(cmd_fn=Maintainers._path_cmd)
-
-    commits_parser = subparsers.add_parser(
-        "commits",
-        help="List area(s) for commit range")
-    commits_parser.add_argument(
-        "commits",
-        metavar="COMMIT_RANGE",
-        nargs="*",
-        help="Commit range to list areas for (default: HEAD~..)")
-    commits_parser.set_defaults(cmd_fn=Maintainers._commits_cmd)
-
-    list_parser = subparsers.add_parser(
-        "list",
-        help="List files in areas")
-    list_parser.add_argument(
-        "area",
-        metavar="AREA",
-        nargs="?",
-        help="Name of area to list files in. If not specified, all "
-             "non-orphaned files are listed (all files that do not appear in "
-             "any area).")
-    list_parser.set_defaults(cmd_fn=Maintainers._list_cmd)
-
-    areas_parser = subparsers.add_parser(
-        "areas",
-        help="List areas and maintainers")
-    areas_parser.add_argument(
-        "maintainer",
-        metavar="MAINTAINER",
-        nargs="?",
-        help="List all areas maintained by maintaier.")
-
-    areas_parser.set_defaults(cmd_fn=Maintainers._areas_cmd)
-
-    orphaned_parser = subparsers.add_parser(
-        "orphaned",
-        help="List orphaned files (files that do not appear in any area)")
-    orphaned_parser.add_argument(
-        "path",
-        metavar="PATH",
-        nargs="?",
-        help="Limit to files under PATH")
-    orphaned_parser.set_defaults(cmd_fn=Maintainers._orphaned_cmd)
-
-    args = parser.parse_args()
-    if not hasattr(args, "cmd_fn"):
-        # Called without a subcommand
-        sys.exit(parser.format_usage().rstrip())
-
-    return args
-
 
 class Maintainers:
     """
@@ -434,7 +346,7 @@ def _load_maintainers(path):
 def _check_maintainers(maints_path, yaml):
     # Checks the maintainers data in 'yaml', which comes from the maintainers
     # file at maints_path, which is a pathlib.Path instance
-
+    return
     root = maints_path.parent
 
     def ferr(msg):
@@ -535,6 +447,3 @@ class MaintainersError(Exception):
 class GitError(Exception):
     "Exception raised for Git-related errors"
 
-
-if __name__ == "__main__":
-    _main()
