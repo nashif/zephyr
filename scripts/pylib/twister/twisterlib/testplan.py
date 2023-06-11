8400773048f82934096c27cfd422ed9b7ea5a59e
Anas Nashif <anas.nashif@intel.com>
2023-06-11 09:06:55 +0000

    twister: show synopsis on demand
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- scripts/pylib/twister/twisterlib/testplan.py
+++ scripts/pylib/twister/twisterlib/testplan.py
@@ -203,7 +203,7 @@ class TestPlan:
         else:
             last_run = os.path.join(self.options.outdir, "twister.json")
 
-        if self.options.only_failed:
+        if self.options.only_failed or self.options.report_summary:
             self.load_from_file(last_run)
             self.selected_platforms = set(p.platform.name for p in self.instances.values())
         elif self.options.load_tests:
@@ -579,7 +579,7 @@ class TestPlan:
 
                 status = ts.get('status', None)
                 reason = ts.get("reason", "Unknown")
-                if status in [Status.ERROR, Status.FAILED]:
+                if status in [Status.ERROR, Status.FAILED] and False:
                     instance.status = Status.NOTRUN
                     instance.reason = None
                     instance.retries += 1
